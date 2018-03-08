a9f741839d660e78f1ab4196569f18f65a1bff1c
Rohit Thapliyal <rohitthapliyal2000@gmail.com>
2018-03-08 11:40:37 +0530

    Showing the progress while fetching / training

---
+++ src/progress_bar.py
@@ -0,0 +1,16 @@
+import sys
+
+def progress(count, total, cond=False):
+
+	bar_len = 60
+	filled_len = int(round(bar_len * count / float(total)))
+
+	percents = round(100.0 * count / float(total), 1)
+	bar = '=' * filled_len + '-' * (bar_len - filled_len)
+
+	if cond == False:
+		sys.stdout.write('[%s] %s%s\r' % (bar, percents, '%'))
+		sys.stdout.flush()
+
+	else:
+		sys.stdout.write('[%s] %s%s' % (bar, percents, '%'))
