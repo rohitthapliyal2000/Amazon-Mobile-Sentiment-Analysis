88e7167ec87b4d31b819751564857cf3344d892f
Rohit Thapliyal <rohitthapliyal2000@gmail.com>
2018-03-08 11:30:13 +0530

    Driver code added

--- src/driver.py
+++ src/driver.py
@@ -1,8 +1,16 @@
 import search
+import training_module
 import reviews
+import sys
 
-info = input("Enter info : ")
+info = input("\nEnter info : ")
 
 review_tab = search.get_first_from_search(info)
 
-review_info = reviews.get_reviews(review_tab)
+if (review_tab == ""):
+	print("Error while scraping\nPossible errors : Incorrect input or connectivity problem\n")
+	sys.exit(0)
+
+review_list = reviews.get_reviews(review_tab)
+
+training_module.sentiment_analysis(review_list)
