11857b0f0e9a37c21ed0f5779c1d3dc87e0e602e
Rohit Thapliyal <rohitthapliyal2000@gmail.com>
2018-03-08 11:34:31 +0530

    Driver code added

--- src/driver.py
+++ src/driver.py
@@ -8,7 +8,7 @@ info = input("\nEnter info : ")
 review_tab = search.get_first_from_search(info)
 
 if (review_tab == ""):
-	print("Error while scraping\nPossible errors : Incorrect input or connectivity problem\n")
+	print("\nError\nPossible errors : Incorrect input/Connectivity problem/Product irrelevant\n")
 	sys.exit(0)
 
 review_list = reviews.get_reviews(review_tab)
